5cc7ccbbca78401952d1b5a9c50fb3613f8dee4a
Matt Giuca <mgiuca@chromium.org>
2015-07-13 17:34:31 +1000

    webactions-polyfill.js: Implement the CustomEventTarget class.

--- polyfill/webactions-polyfill.js
+++ polyfill/webactions-polyfill.js
@@ -20,4 +20,64 @@ if (Cache.prototype.addAll === undefined) {
   }
 }
 
+// A base class that implements the EventTarget interface.
+function CustomEventTarget() {
+  // Map from event type to list of listeners.
+  this._listeners = {};
+}
+CustomEventTarget.prototype = Object.create(EventTarget.prototype);
+
+CustomEventTarget.prototype.addEventListener = function(type, listener,
+                                                        useCapture) {
+  var listeners = this._listeners;
+  var listeners_of_type = listeners[type];
+  if (listeners_of_type === undefined) {
+    listeners[type] = listeners_of_type = [];
+  }
+
+  for (var i = 0; i < listeners_of_type.length; i++) {
+    if (listeners_of_type[i] === listener)
+      return;
+  }
+
+  listeners_of_type.push(listener);
+};
+
+CustomEventTarget.prototype.removeEventListener = function(type, listener,
+                                                           useCapture) {
+  var listeners = this._listeners;
+  var listeners_of_type = listeners[type];
+  if (listeners_of_type === undefined) {
+    listeners[type] = listeners_of_type = [];
+  }
+
+  for (var i = 0; i < listeners_of_type.length; i++) {
+    if (listeners_of_type[i] === listener) {
+      listeners_of_type.splice(i, 1);
+
+      if (listeners_of_type.length == 0)
+        delete listeners[type];
+
+      return;
+    }
+  }
+};
+
+CustomEventTarget.prototype.dispatchEvent = function(evt) {
+  var listeners = this._listeners;
+  var listeners_of_type = listeners[evt.type];
+  if (listeners_of_type === undefined) {
+    listeners[evt.type] = listeners_of_type = [];
+  }
+
+  for (var i = 0; i < listeners_of_type.length; i++) {
+    var listener = listeners_of_type[i];
+    if (listener.handleEvent !== undefined) {
+      listener.handleEvent(evt);
+    } else {
+      listener.call(evt.target, evt);
+    }
+  }
+};
+
 })();
