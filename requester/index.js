49b3386c0accf73a94808c47e3de871b45ccb6e7
Matt Giuca <mgiuca@chromium.org>
2015-08-26 13:30:45 +1000

    requester: Add a service worker (currently does nothing).

--- requester/index.js
+++ requester/index.js
@@ -83,6 +83,17 @@ function debugCloseAction(action) {
 }
 
 function onLoad() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js').then(registration => {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ',
+                  registration.scope);
+    }).catch(err => {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  }
+
   document.getElementById('edit_button')
       .addEventListener('click', editButtonClick);
 
