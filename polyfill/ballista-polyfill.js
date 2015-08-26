6dd13b740695b9af94d9f1d082683453e2a4fefa
Matt Giuca <mgiuca@chromium.org>
2015-08-26 15:05:46 +1000

    polyfill: performAction can now be called from a service worker.
    It requires that some foreground page belonging to the requester be open
    (just for implementation reasons). This is not an intended limitation of
    the API, just a limitation of the polyfill.

--- polyfill/ballista-polyfill.js
+++ polyfill/ballista-polyfill.js
@@ -69,19 +69,57 @@ function onMessage(event) {
     }
     event.stopImmediatePropagation();
     return;
+  } else if (data.type == 'sendPortToHandler') {
+    sendPortToHandler(data.url, data.port);
   }
 }
+
+var findLastTopLevelClient = undefined;
 
 // Listen for 'fetch' and 'message', if in a service worker.
 if (self.WorkerNavigator !== undefined) {
   self.addEventListener('fetch', onFetch);
   self.addEventListener('message', onMessage);
+
+  // Finds the most recently opened top-level client belonging to this service
+  // worker. Returns a promise. Rejects the promise if none found.
+  findLastTopLevelClient = () => {
+    return new Promise((resolve, reject) => {
+      clients.matchAll().then(allClients => {
+        for (var i = allClients.length - 1; i >= 0; i--) {
+          if (allClients[i].frameType == 'top-level') {
+            resolve(allClients[i]);
+            return;
+          }
+        }
+        reject(new Error('No available clients; please open a tab.'));
+      });
+    });
+  }
+} else {
+  // Receive messages from the service worker.
+  navigator.serviceWorker.addEventListener('message', onMessage);
 }
 
 // Establishes a connection with the polyfill handler (by embedding a page from
 // the handler's domain in an iframe), and posts a MessagePort object to it.
 // Asynchronous; no return value.
 function sendPortToHandler(url, port) {
+  if (self.document === undefined) {
+    // We are in a service worker. No way to create an iframe here (needed to
+    // establish a connection to handler service worker), so connect to a random
+    // foreground page and establish a connection there (it does not need to be
+    // the same foreground page that initiated the action, as long as it is
+    // running this polyfill code). If there is no open requester foreground
+    // page, this will fail. This *should not* happen in the real API, but there
+    // is no way around this in the polyfill.
+    findLastTopLevelClient().then(client => {
+      client.postMessage({type: 'sendPortToHandler', url: url, port: port},
+                         [port]);
+    });
+    return;
+  }
+
   var slashIdx = url.indexOf('/', 10);
   var origin = url.substr(0, slashIdx);
   var iframe = document.createElement('iframe');
